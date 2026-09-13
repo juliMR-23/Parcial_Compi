e958caecd6952c65d4b3d38d13f1a322f4684fbf
juliMR-23 <julian.marin42@eia.edu.co>
2026-09-13 14:46:35 -0500

    feat: clase edge, base para todo el código de algoritmos

--- src/models/Edge.java
+++ src/models/Edge.java
@@ -1,5 +1,29 @@
 package models;
 
-public class Edge {
-    // TODO: Implementar por el usuario
+public class Edge implements Comparable<Edge> {
+    ///directed Edge
+    private int source, target, weight;
+    public Edge(int source, int target, int weight) {
+        this.source = source;
+        this.target = target;
+        this.weight = weight;
+        //weight is int for simplicity
+        //long just for accumulations (in the algorithms)
+    }
+
+    //getters
+    public int getSource() {
+        return source;
+    }
+    public int getTarget() {
+        return target;
+    }
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override //comparable edges, will be used for kruskal
+    public int compareTo(Edge o) {
+        return Integer.compare(weight, o.weight); // compare weights
+    }
 }
