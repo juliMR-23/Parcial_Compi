daf75e5b318e1dcaf4e8106ac96c0ecf48648637
lazermortal <juanjoseflorezlopez@gmail.com>
2026-09-13 20:47:22 -0500

    creacion de DFS y BFS

--- src/algorithms/BFSSolver.java
+++ src/algorithms/BFSSolver.java
@@ -1,5 +1,60 @@
 package algorithms;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class BFSSolver {
     // TODO: Implementar por el usuario
+
+    private int R;
+    private int C;
+    private final int startRow,  startCol;
+    private final int finalRow,  finalCol;
+    private final boolean[][] bomb;
+
+    public BFSSolver(int R, int C,int startRow, int startCol, boolean[][] bomb, int finalRow, int finalCol) {
+        this.R = R;
+        this.C = C;
+        this.startRow = startRow;
+        this.startCol = startCol;
+        this.finalRow = finalRow;
+        this.finalCol = finalCol;
+        this.bomb = bomb;
+    }
+
+    public int bfs()
+
+    {
+        if (bomb[startRow][startCol] || bomb[finalRow][finalCol]) return -1;
+
+        int[][] dist = new int[R][C];
+        for (int[] row : dist) Arrays.fill(row, -1);
+        Queue<int[]> queue = new LinkedList<int[]>();
+        dist[startRow][startCol] = 0;
+        queue.offer(new int[]{startRow, startCol});
+
+        int[] dRow = {-1,1,0,0};
+        int[] dCol = {0,0,-1,1};
+
+
+        while (!queue.isEmpty()){
+            int[]  cur = queue.poll();
+            for(int i = 0; i < 4; i++){
+                int y = cur[0] + dRow[i];
+                int x = cur[1] + dCol[i];
+                if(y < 0 || y >= R || x < 0 || x >= C ) continue;
+                if(bomb[y][x]) continue;
+                if(dist[y][x] != -1) continue;
+                dist[y][x] = dist[cur[0]][cur[1]] + 1;
+                queue.add(new int[]{y,x});
+            }
+
+        }
+        return dist[finalRow][finalCol];
+
+
+
+    }
 }
--- src/algorithms/DFSSolver.java
+++ src/algorithms/DFSSolver.java
@@ -1,5 +1,48 @@
 package algorithms;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class DFSSolver {
     // TODO: Implementar por el usuario
+
+    private final int R , C;
+    private final int startRow, startCol;
+    private final int finalRow, finalCol;
+    private boolean[][] bomb;
+
+    public DFSSolver(int r, int c, int startRow, int startCol, int finalRow, int finalCol, boolean[][] bomb) {
+        R = r;
+        C = c;
+        this.startRow = startRow;
+        this.startCol = startCol;
+        this.finalRow = finalRow;
+        this.finalCol = finalCol;
+        this.bomb = bomb;
+    }
+
+    private int dfs(){
+        if(bomb[startRow][startCol]||bomb[finalRow][finalCol]) return -1;
+
+        boolean[][] visited = new boolean[R][C];
+        visited[startRow][startCol] = true;
+        Deque<int[]> deque = new ArrayDeque<>();
+        deque.push(new int[]{startRow, startCol,0});
+        int[] dRow = {-1,1,0,0};
+        int[] dCol = {0,0,-1,1};
+        while(!deque.isEmpty()){
+            int[] cur = deque.pop();
+            if(cur[0]== finalRow && cur[1]==finalCol) return cur[2] ;
+            for(int i = 3; i >= 0; i--){
+                int y =  cur[0] + dRow[i];
+                int x = cur[1] + dCol[i];
+                if(y < 0 || y >= R || x < 0 || x >= C) continue;
+                if(visited[y][x]) continue;
+                if(bomb[y][x]) continue;
+                visited[y][x] = true;
+                deque.push(new int[]{y,x,cur[2]+1});
+            }
+        }
+        return -1 ;
+    }
 }
